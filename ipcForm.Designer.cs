97b1231ec1ec5358b5593389c938f11144db42c9
Imnotdesh <slateinbox@gmail.com>
2024-12-02 12:19:18 +0300

    Added IPC to roject

--- ipcForm.Designer.cs
+++ ipcForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace winOsManagement
 {
-    partial class Form1
+    partial class ipcForm
     {
         /// <summary>
         /// Required designer variable.
@@ -31,11 +31,8 @@ namespace winOsManagement
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -54,81 +51,47 @@ namespace winOsManagement
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.button5);
-            this.splitContainer1.Panel2.Controls.Add(this.button4);
             this.splitContainer1.Panel2.Controls.Add(this.button3);
             this.splitContainer1.Panel2.Controls.Add(this.button2);
-            this.splitContainer1.Panel2.Controls.Add(this.button1);
             this.splitContainer1.Size = new System.Drawing.Size(800, 450);
-            this.splitContainer1.SplitterDistance = 520;
+            this.splitContainer1.SplitterDistance = 378;
             this.splitContainer1.TabIndex = 0;
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(20, 161);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 187);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(477, 103);
+            this.label1.Size = new System.Drawing.Size(237, 65);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Dashboard";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(12, 204);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(250, 59);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "I/O Management";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label1.Text = "Inter-Process Communication";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 109);
+            this.button2.Location = new System.Drawing.Point(91, 77);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(251, 67);
+            this.button2.Size = new System.Drawing.Size(237, 58);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Process Management";
+            this.button2.Text = "Demo SOCKET communication";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button1
+            // button3
             // 
-            this.button1.Location = new System.Drawing.Point(12, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(252, 59);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "File Management";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button3.Location = new System.Drawing.Point(93, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(235, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Demo PIPE communication";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(12, 284);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(249, 50);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Memory Management";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(12, 364);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(248, 48);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Interprocess Communication";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // Form1
+            // ipcForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.splitContainer1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "ipcForm";
+            this.Text = "Form2";
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -141,11 +104,7 @@ namespace winOsManagement
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
     }
 }
-
